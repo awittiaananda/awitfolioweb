7db152efae241c79e07954659aae49016204009b
awiti444 <andayanidini803@gmail.com>
2024-05-10 13:26:57 +0700

    website portfolio awit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ navlist.addEventListener("click",()=>{
 
 
 // rotate text js code 
-let text = document.querySelector(".text p");
+let text = document.querySelector(".text");
 
 text.innerHTML = text.innerHTML.split("").map((char,i)=>
     `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
